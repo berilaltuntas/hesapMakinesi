5c1cdd1e1ee6ce56b8e381a6e03114d31a2ed3fb
berilaltuntas <162134585+berilaltuntas@users.noreply.github.com>
2024-03-13 21:14:47 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        int n1, n2, select;
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("İlk sayıyı giriniz: ");
+        n1 = input.nextInt();
+
+        System.out.print("İkinci sayıyı giriniz: ");
+        n2 = input.nextInt();
+
+        //Kullanıcıdan sayılar alındı, yapmak istediği işlemi soruluyor.
+
+        System.out.println("Yapmak istediğiniz işlemi seçiniz: ");
+        System.out.println("1-Toplama\n2-Çıkarma\n3-Çarpma\n4-Bölme ");
+        System.out.print("Seçiminiz: ");
+        select = input.nextInt();
+
+        switch (select) {
+            case 1:
+                System.out.println("İşleminizin sonucu: " + (n1 + n2));
+                break;
+            case 2:
+                System.out.println("İşleminizin sonucu: " + (n1 - n2));
+                break;
+            case 3:
+                System.out.println("İşleminizin sonucu: " + (n1 * n2));
+                break;
+            case 4:
+                if (n2 != 0) {
+                    System.out.println("İşleminizin sonucu: " + (n1 / n2));
+                } else {
+                    System.out.println("Bir sayı 0'a bölünmez!");
+                }
+                break;
+            default:
+                System.out.println("Hatalı bir seçim yaptınız.Tekrar deneyiniz.");
+        }
+    }
+}
